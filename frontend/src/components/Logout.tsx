74d0ce9223b164d5b67c414fc64afae3b6e71405
mihirkate <mihirkate13@gmail.com>
2024-07-05 04:20:52 +0530

    added logout component

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -1,7 +1,8 @@
 // Example with React
 import React from 'react';
+
 import { useNavigate } from 'react-router-dom';
-const LogoutButton = () => {
+export const LogoutButton = () => {
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -20,4 +21,4 @@ const LogoutButton = () => {
     );
 };
 
-export default LogoutButton;
+
