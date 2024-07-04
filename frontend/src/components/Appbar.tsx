fa11130ec121f6579b775c7956b37e9f2429da30
mihirkate <mihirkate13@gmail.com>
2024-07-05 04:17:07 +0530

    added logout component

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,5 +1,6 @@
 import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
+import LogoutButton from "./Logout"
 
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-4">
@@ -12,6 +13,9 @@ export const Appbar = () => {
                 className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">
                     New
                 </button>
+            </Link>
+            <Link to={"/signin"} >
+            <LogoutButton/>
             </Link>
 
             <Avatar size={"big"} name="harkirat" />
---
+++ frontend/src/components/Logout.tsx
@@ -0,0 +1,23 @@
+// Example with React
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+const LogoutButton = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Remove JWT from local storage or cookies
+        localStorage.removeItem('token'); // Adjust based on your storage mechanism
+
+        // Redirect to sign-in page
+        navigate('/signin');
+    };
+
+    return (
+
+        <button onClick={handleLogout} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+            Logout
+        </button>
+    );
+};
+
+export default LogoutButton;
