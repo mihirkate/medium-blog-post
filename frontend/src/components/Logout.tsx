bf33d7fac1d68cac9d08baa44c3dd7d818b0e786
mihirkate <mihirkate13@gmail.com>
2025-05-12 20:57:13 +0530

    changes

--- frontend/src/components/Logout.tsx
+++ frontend/src/components/Logout.tsx
@@ -1,7 +1,7 @@
 // Example with React
 
 import { useNavigate } from 'react-router-dom';
-export const LogoutButton = () => {
+const LogoutButton = () => {
     const navigate = useNavigate();
 
     const handleLogout = () => {
@@ -13,11 +13,12 @@ export const LogoutButton = () => {
     };
 
     return (
-
         <button onClick={handleLogout} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
             Logout
         </button>
     );
 };
+
+export default LogoutButton;
 
 
